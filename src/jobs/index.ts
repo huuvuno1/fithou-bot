a24230b77cac6893f6a3441cb6b69488244b890a
huuvuno1 <nguyenhuuvuno1@gmail.com>
2022-09-03 17:41:17 +0700

    feat: add cron job ctms

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,5 +1,10 @@
-import schedule from 'node-schedule';
 import config from 'config';
+import schedule from 'node-schedule';
+import { ToadScheduler } from 'toad-scheduler';
+import job from './crawlCtmsSubject';
+import crawlCtmsJob from './crawlCtmsSubject';
+
+const scheduler = new ToadScheduler();
 
 const rule = new schedule.RecurrenceRule();
 rule.minute = config.jobs.fithou.minute;
@@ -8,6 +13,8 @@ import { crawlFithouJob } from './crawlFithouJob';
 
 const runjobs = () => {
   schedule.scheduleJob(rule, crawlFithouJob);
+  scheduler.addSimpleIntervalJob(crawlCtmsJob);
+  console.log(scheduler.getById(job.id).getStatus());
 };
 
 export default runjobs;
--- src/services/facebook/index.ts
+++ src/services/facebook/index.ts
@@ -1,5 +1,5 @@
 import logger from 'logger';
-import { ArticlesModel } from 'models';
+import { ArticlesModel, UserModel } from 'models';
 import { convertHtmlToImage, deleteImage, getSubjects, getUserID, logoutCtms } from 'services/ctms';
 import config from '../../config';
 const { default: axios } = require('axios');
@@ -46,39 +46,99 @@ const sendLoginCtmsButton = (id: string) => {
   });
 };
 
-const sendSubjectCtms = async (receiver: string, cookie: Array<string>) => {
+const sendSubjectCtms = async (receiver: string | string[], cookie: Array<string>, username: string) => {
+  const user = await UserModel.findOne({ username });
+  if (typeof receiver === 'string' && user.subjectHTML !== '') {
+    const data = await convertHtmlToImage(user.subjectHTML);
+    sendMessage(receiver, {
+      attachment: {
+        type: 'image',
+        payload: {
+          url: config.host + '/' + data.image,
+        },
+      },
+    });
+    return;
+  }
+
   const id = await getUserID(cookie);
   const subjects = await getSubjects(cookie, id);
+  if (subjects === null || user.subjectHTML === subjects) {
+    logoutCtms(cookie);
+    return;
+  }
   const data = await convertHtmlToImage(subjects);
   setTimeout(() => {
     deleteImage(data.image);
   }, 1000 * 60 * 2);
-  sendMessage(receiver, {
-    text: `Dưới đây là các môn bạn hiện tại bạn có thể đăng ký. \nBot sẽ gửi thông báo cho bạn khi có thay đổi.`,
-  });
-  sendMessage(receiver, {
-    attachment: {
-      type: 'image',
-      payload: {
-        url: config.host + '/' + data.image,
+  UserModel.updateOne({ username }, { subjectHTML: subjects }).then();
+
+  if (typeof receiver === 'string') {
+    receiver = [receiver];
+  } else {
+    user.subscribedIDs.forEach((subID) => {
+      sendMessage(subID, {
+        text: `Hú hú ${username} có tín chỉ mới.`,
+      });
+    });
+  }
+
+  receiver.forEach((receiver_id: string) => {
+    sendMessage(receiver_id, {
+      attachment: {
+        type: 'image',
+        payload: {
+          url: config.host + '/' + data.image,
+        },
       },
-    },
+    });
   });
   logoutCtms(cookie);
 };
 
 const subscribedFithouNotification = async (receiver: string) => {
-  sendMessage(receiver, {
+  const article = await ArticlesModel.findOne({});
+  if (article.subscribedIDs.indexOf(receiver) !== -1) {
+    await sendMessage(receiver, {
+      text: `Bạn đã đăng ký nhận thông báo từ Fithou rồi.`,
+    });
+    return;
+  }
+  article.subscribedIDs.push(receiver);
+  await article.save();
+  await sendMessage(receiver, {
     text: `Đăng ký nhận bài viết mới từ Fithou thành công.`,
   });
-  const article = await ArticlesModel.findOne({});
-  if (article.subscribedIDs.indexOf(receiver) === -1) {
-    article.subscribedIDs.push(receiver);
-    await article.save();
-  }
-  sendMessage(receiver, {
+  await sendMessage(receiver, {
     text: `Gửi bạn bài viết mới nhất hiện tại. Bot sẽ câp nhật thông báo khi có bài viết mới.\n${article.link}`,
   });
 };
 
-export { sendMessage, sendLoginCtmsButton, sendSubjectCtms, subscribedFithouNotification };
+const unsubCtmsNotification = async (receiver: string) => {
+  sendMessage(receiver, {
+    text: `Đã hủy nhận thông báo từ CTMS.`,
+  });
+  const user = await UserModel.findOne({ subscribedIDs: receiver });
+  if (user) {
+    user.subscribedIDs = user.subscribedIDs.filter((id) => id !== receiver);
+    await user.save();
+  }
+};
+
+const unsubFithouNotification = async (receiver: string) => {
+  sendMessage(receiver, {
+    text: `Đã hủy nhận thông báo từ Fithou.`,
+  });
+  const article = await ArticlesModel.findOne({});
+  article.subscribedIDs = article.subscribedIDs.filter((id) => id !== receiver);
+  await article.save();
+};
+
+export {
+  sendMessage,
+  sendLoginCtmsButton,
+  sendSubjectCtms,
+  subscribedFithouNotification,
+  unsubCtmsNotification,
+  unsubFithouNotification,
+};
