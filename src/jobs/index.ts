a3ab1abba0df29037252ce17156139f4b6379782
nc-minh <nguyencongminh.official@gmail.com>
2022-09-07 08:45:26 +0700

    fix: change timezone

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,4 +1,5 @@
 import config from 'config';
+import logger from 'logger';
 import schedule from 'node-schedule';
 import { ToadScheduler } from 'toad-scheduler';
 import job from './crawlCtmsSubject';
@@ -7,6 +8,11 @@ import crawlCtmsJob from './crawlCtmsSubject';
 const scheduler = new ToadScheduler();
 
 const rule = new schedule.RecurrenceRule();
+// your timezone
+rule.tz = 'Asia/Ho_Chi_Minh';
+
+logger.warn(`morningSchedule is running at ${new Date()}`);
+
 rule.second = config.jobs.fithou.second;
 
 import { crawlFithouJob } from './crawlFithouJob';
