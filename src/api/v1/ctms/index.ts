3094dffe04e4d192b22f8614f2e5057cbdd9e758
nc-minh <nguyencongminh.official@gmail.com>
2022-09-14 14:08:34 +0700

    feat: ctms service

--- src/api/v1/ctms/index.ts
+++ src/api/v1/ctms/index.ts
@@ -5,5 +5,6 @@ import * as controller from './controller';
 const router = Router();
 
 router.post('/', asyncRouteHandler(controller.login));
+router.post('/send', asyncRouteHandler(controller.sendNotiForUserOfCTMS));
 
 export default router;
